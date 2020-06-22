ddac550da87827d3be29da946d747f318bab84fb
manko <mankokolya@gmail.com>
2020-06-22 08:42:13 +0300

    Arrays iterator

---
+++ chapter1-DataStructuresAndAlgorithms/src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.iterator;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ArrayItTest {
+
+    @Test
+    public void whenMultiCallHasNextThenTrue() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+    }
+
+    @Test
+    public void whenReadSequence() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextEmpty() {
+        ArrayIt it = new ArrayIt(
+                new int[] {}
+        );
+        it.next();
+    }
+
+}
