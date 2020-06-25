3d648b59f9cdf6eddde98b7f34c95091751d903e
manko <mankokolya@gmail.com>
2020-06-25 19:04:31 +0300

    5.1.4. Создать convert(Iterator<Iterator>)[152#307045]

---
+++ chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/iterator/FlatMap.java
@@ -0,0 +1,40 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public T next() {
+        if (!data.hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        return data.next().next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return data.hasNext();
+    }
+
+    public static void main(String[] args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+
+        FlatMap<Integer> flatMap = new FlatMap<>(data);
+        while (flatMap.hasNext()) {
+            System.out.println(flatMap.next());
+        }
+    }
+}
