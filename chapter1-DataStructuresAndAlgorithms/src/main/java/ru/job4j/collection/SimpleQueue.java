f2ba41942feb5a55d67e3930042634c2f338754f
manko <mankokolya@gmail.com>
2020-07-05 08:55:03 +0300

    5. Очередь на двух стеках[160#307039] corrected method reverseStack() and named it getFirstQueueValue()

--- chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,24 +1,28 @@
 package ru.job4j.collection;
 
+import com.sun.jdi.Value;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
 
     public T poll() {
-        reverseStack(in, out);
-        T firstValue = out.pop();
-        reverseStack(out, in);
-        return firstValue;
+        return getFirstQueueValue();
     }
 
     public void push(T value) {
         in.push(value);
     }
 
-    public void reverseStack(SimpleStack<T> input, SimpleStack<T> output) {
-        while (!input.isEmpty()) {
-            output.push(input.pop());
+    public T getFirstQueueValue() {
+        while (!in.isEmpty()) {
+            out.push(in.pop());
         }
+        T value = out.pop();
+        while (!out.isEmpty()) {
+            in.push(out.pop());
+        }
+        return value;
     }
 }
