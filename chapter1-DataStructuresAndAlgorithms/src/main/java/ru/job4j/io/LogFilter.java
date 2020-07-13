fa3035380f2c2132f46fd450c1fcaf03a5581cd9
Nick <mankokolya@gmail.com>
2020-07-13 14:36:23 +0300

    0.4. BufferedOutputStream[252490#307087]

--- chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 import java.util.*;
 
@@ -9,16 +8,29 @@ public class LogFilter {
     public static List<String> filter(String file) {
         List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines().filter(line->line.contains("404")).forEach(rsl::add);
+            in.lines().filter(line -> line.contains("404")).forEach(rsl::add);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return rsl;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                )
+        )) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        for (String line: log) {
+        save(log, "logFiltered.txt");
+        for (String line : log) {
             System.out.println(line);
         }
     }
