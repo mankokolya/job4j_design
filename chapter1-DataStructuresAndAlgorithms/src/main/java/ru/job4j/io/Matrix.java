46916a2faff861546b63ea0b1c3a1ca483b7ff53
Nick <mankokolya@gmail.com>
2020-07-13 11:52:26 +0300

    0.1. FileOutputStream.[252488#307085]

---
+++ chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+/**
+ * Matrix - is used write multidimensional array of numbers to file by multiplying indexes on each other;
+ *
+ * @author mankokolya;
+ */
+public class Matrix {
+    /**
+     * @param size - the size of the multidimensional array;
+     *
+     */
+    public static void multiple(int size) {
+        try (FileOutputStream out = new FileOutputStream("multiplication.txt")) {
+            for (int i = 1; i <= size; i++) {
+                for (int j = 1; j <= size; j++) {
+                    out.write(((i * j) + "\t").getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
