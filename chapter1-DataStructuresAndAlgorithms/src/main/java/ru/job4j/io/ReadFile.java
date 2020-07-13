4edc1abaf2b4d92ec7e51db0bbfd2e5c8cceb1b1
Nick <mankokolya@gmail.com>
2020-07-13 13:52:12 +0300

    0.3. BufferedReader.[252489#307086]

--- chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/io/ReadFile.java
+++ chapter1-DataStructuresAndAlgorithms/src/main/java/ru/job4j/io/ReadFile.java
@@ -1,20 +1,17 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            System.out.println(text);
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            List<String> lines = new ArrayList<>();
+            in.lines().forEach(lines::add);
+            for (String  line: lines) {
                 System.out.println(line);
-
             }
         } catch (Exception e) {
             e.printStackTrace();
