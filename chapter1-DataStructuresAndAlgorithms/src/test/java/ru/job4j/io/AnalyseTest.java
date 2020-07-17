d68885ae1e5fc5b9550fda76cb53d04f8f2b01b2
Nick <mankokolya@gmail.com>
2020-07-17 11:00:08 +0300

    3.0. Тестирование IO[173905#307083]

--- chapter1-DataStructuresAndAlgorithms/src/test/java/ru/job4j/io/AnalyseTest.java
+++ chapter1-DataStructuresAndAlgorithms/src/test/java/ru/job4j/io/AnalyseTest.java
@@ -1,21 +1,34 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.StringJoiner;
 
 import static org.junit.Assert.*;
 
 public class AnalyseTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
     private final Analyse analyse = new Analyse();
 
     @Test
-    public void analyse() {
-        analyse.unavailable("./data/server.txt", "./data/unavailable.csv");
+    public void analyse() throws IOException {
+        File source = folder.newFile("server.txt");
+        File target = folder.newFile("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator()
+                    + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator()
+                    + "200 11:02:02");
+        }
+        analyse.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringJoiner dataFromFile = new StringJoiner(System.lineSeparator());
-        try (BufferedReader in = new BufferedReader(new FileReader("./data/unavailable.csv"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(target.getAbsoluteFile()))) {
             in.lines().forEach(dataFromFile::add);
         } catch (Exception e) {
             e.printStackTrace();
