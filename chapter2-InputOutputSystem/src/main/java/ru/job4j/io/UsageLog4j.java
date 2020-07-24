00bba05b702268e49ec8d108a864b0814f77c4af
Nick <mankokolya@gmail.com>
2020-07-24 14:39:57 +0300

    1. Log4j. Логирование системы.[95335#307077] Corrected Logger.getLogger to LogManager.getLogger

--- chapter2-InputOutputSystem/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter2-InputOutputSystem/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,12 +1,12 @@
 package ru.job4j.io;
 
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import java.util.logging.LogManager;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = Logger.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
