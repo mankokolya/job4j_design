f4acb5a6d428f3cfd0647d2766dc933e95d1e204
manko <mankokolya@gmail.com>
2020-08-22 12:10:30 +0300

    1. Схема прав пользователи и роли.[1093#307017]

--- chapter3-DataBases/Data/create.sql
+++ chapter3-DataBases/Data/create.sql
@@ -1,30 +1,75 @@
-CREATE DATABASE orders;
-GO orders;
+drop DATABASE IF EXISTS orders;
 
-CREATE TABLE rules(
-    rules_uuid UUID NOT NULL PRIMARY KEY
+create DATABASE orders;
+\c orders;
+CREATE EXTENSION "uuid-ossp";
+
+create table rules(
+    rules_uuid uuid default uuid_generate_v4 (),
+    rule varchar(200) not null,
+    primary key(rules_uuid)
 );
-CREATE TABLE role (
-    role_uuid UUID NOT NULL PRIMARY KEY
+
+create table role (
+    role_uuid uuid default uuid_generate_v4 (),
+    role varchar(100) not null,
+    primary key(role_uuid)
 );
-CREATE TABLE role_rules(
-    role_rules_uuid UUID NOT NULL PRIMARY KEY
+
+create table role_rules(
+    role_rules_uuid uuid default uuid_generate_v4 (),
+    role_uuid uuid references role(role_uuid),
+    rules_uuid uuid references rules(rules_uuid),
+    primary key(role_rules_uuid)
 );
-CREATE TABLE customer (
-    user_uuid UUID NOT NULL PRIMARY KEY
+
+create table customer (
+    customer_uuid uuid default uuid_generate_v4 (),
+    first_name varchar(100) not null,
+    last_name varchar(100) not null,
+    email varchar(200),
+    role_uuid uuid references role(role_uuid),
+    primary key (customer_uuid)
 );
-CREATE TABLE state (
-    state_uuid UUID NOT NULL PRIMARY KEY
+
+create table state (
+    state_uuid uuid default uuid_generate_v4 (),
+    condition varchar(100) not null,
+    primary key (state_uuid)
 );
-CREATE TABLE comments (
-    comments_uuid UUID NOT NULL PRIMARY KEY
+
+create table category (
+    category_uuid uuid default uuid_generate_v4 (),
+    category varchar(100) not null,
+    primary key (category_uuid)
 );
-CREATE TABLE attachment (
-    attachment_uuid UUID NOT NULL PRIMARY KEY
+
+create table item (
+    item_uuid uuid default uuid_generate_v4 (),
+    customer_uuid uuid references customer(customer_uuid),
+    state_uuid uuid references state(state_uuid),
+    category_uuid uuid references category(category_uuid),
+    primary key (item_uuid)
 );
-CREATE TABLE category (
-    category_uuid UUID NOT NULL PRIMARY KEY
+
+create table comments (
+    comments_uuid uuid default uuid_generate_v4 (),
+    comment varchar(200) not null,
+    item_uuid uuid references item(item_uuid),
+    primary key (comments_uuid)
 );
-CREATE TABLE item (
-    item_uuid UUID NOT NULL PRIMARY KEY
+
+create table attachments (
+    attachment_uuid uuid default uuid_generate_v4 (),
+    attachment varchar(200),
+    item_uuid uuid references item(item_uuid),
+    primary  key (attachment_uuid)
 );
+
+insert into rules(rule) values ('Implement');
+insert into role(role) values ('Ingineer');
+insert into customer(first_name, last_name, email) values ('Nick', 'Manko', 'mankokolya@gmail.com');
+insert into state(condition) values ('in progress');
+insert into category(category) values ('website');
+insert into comments(comment) values ('waiting for frontend implementation');
+insert into attachments(attachment) values ('https://drive.google.com/file/d/15O8L1bCHwz2h8peODV2dAv4O3Jr3Olji/view?usp=sharing');
