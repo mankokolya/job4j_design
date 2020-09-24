41d8eb949f292fe21bc5e9dd89f7111d7f402f6c
manko <mankokolya@gmail.com>
2020-09-24 20:39:59 +0300

    Реализации кеша на SoftReference[1592#307139]

--- chapter4-JvmAndJmm/src/main/java/cache/ReadTextFromPC.java
+++ chapter4-JvmAndJmm/src/main/java/cache/ReadTextFromPC.java
@@ -6,17 +6,23 @@ import java.util.stream.Collectors;
 
 public class ReadTextFromPC {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SoftReferenceCache<String, String> filesFromPc = new SoftReferenceCache<>();
         String namesFile = "./chapter4-JvmAndJmm/src/main/resources/Names.txt";
         String addressesFile = "./chapter4-JvmAndJmm/src/main/resources/Addresses.txt";
 
-        Function<BufferedReader, String> f =
-                (BufferedReader b) -> {
-                    return b.lines().collect(Collectors.joining(System.lineSeparator()));
-                };
-
-        System.out.println(filesFromPc.get(namesFile, f);
+        Function<String, String> f = str -> {
+            String st = null;
+            try (BufferedReader reader = new BufferedReader(new FileReader(str))) {
+                st = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return st;
+        };
+//        filesFromPc.put(namesFile, f);
+        System.out.println(filesFromPc.get(namesFile, f));
+        System.out.println(filesFromPc.get(addressesFile, f));
 
     }
 }
--- chapter4-JvmAndJmm/src/main/java/cache/SoftReferenceCache.java
+++ chapter4-JvmAndJmm/src/main/java/cache/SoftReferenceCache.java
@@ -3,22 +3,20 @@ package cache;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class SoftReferenceCache<K, V> {
-    private final HashMap<K, SoftReference<V>> cache = new HashMap<>();;
+    private final HashMap<K, SoftReference<V>> cache = new HashMap<>();
 
-    public void put(K key, V value) {
-        cache.put(key, new SoftReference<>(value));
+    public void put(K key, Function<K, V> getValue) {
+        cache.put(key, new SoftReference<>(getValue.apply(key)));
     }
 
     public V get(K key, Function<K, V> getValue) {
         SoftReference<V> reference = cache.get(key);
-        if (reference != null ) {
+        if (reference != null) {
             return reference.get();
         }
-        V value = getValue.apply(key);
-        this.put(key, value);
-        return value;
+        this.put(key, getValue);
+        return this.cache.get(key).get();
     }
 }
