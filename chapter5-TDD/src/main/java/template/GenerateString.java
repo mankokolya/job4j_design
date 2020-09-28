effa1739d5fccc9f23a48e78484ec4cdb5f2d696
manko kolya <mankokolya@gmail.com>
2020-09-28 15:10:33 +0300

    3. Шаблонизатор.[855#307105]

---
+++ chapter5-TDD/src/main/java/template/GenerateString.java
@@ -0,0 +1,11 @@
+package template;
+
+import java.util.Map;
+
+public class GenerateString implements Generator {
+
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
