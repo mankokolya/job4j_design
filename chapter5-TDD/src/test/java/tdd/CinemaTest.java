5d17707b557b3f5f8755085578f21fa3d30b4814
manko kolya <mankokolya@gmail.com>
2020-09-30 16:02:01 +0300

    2. Что такое TDD?[4918#307106]  Moved field initialization to test

--- chapter5-TDD/src/test/java/tdd/CinemaTest.java
+++ chapter5-TDD/src/test/java/tdd/CinemaTest.java
@@ -10,9 +10,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CinemaTest {
-    private final Cinema cinema3D = new Cinema3D();
     @Test
     public void buy() {
+        Cinema cinema3D = new Cinema3D();
         Account account = new AccountCinema();
         Calendar date = Calendar.getInstance();
         date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
@@ -22,6 +22,7 @@ public class CinemaTest {
 
     @Test
     public void find() {
+        Cinema cinema3D = new Cinema3D();
         cinema3D.add(new Session3D());
         List<Session> sessions = cinema3D.find(session -> true);
         assertThat(sessions, is(Arrays.asList(new Session3D())));
@@ -29,6 +30,7 @@ public class CinemaTest {
 
     @Test
     public void add(Session session) {
+        Cinema cinema3D = new Cinema3D();
         cinema3D.add(session);
         assertTrue(cinema3D.find(session1 -> true).contains(session));
     }
